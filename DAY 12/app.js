620b2afadcbddbe41a548f7e9f50229da1bb4bc7
Julius Adebowale <juadebgabriel@gmail.com>
2022-11-21 00:01:57 -0600

    practice

--- DAY 12/app.js
+++ DAY 12/app.js
@@ -1,3 +1,44 @@
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// ## Array Cardio Day 2
+
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const inventors = [
+  { firstName: "Wes", secondName: "Baloo", year: 1988 },
+  { firstName: "Kait", secondName: "Baloo", year: 1986 },
+  { firstName: "Irv", secondName: "Baloo", year: 1970 },
+  { firstName: "Lux", secondName: "Baloo", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+// Array.filter
+const old = people.filter(
+  (people) => people.year >= 1970 && people.year < 1990
+);
+console.table(old);
+
+// Array.map
+const fullNames = inventors.map(
+  (inventors) => `${inventors.firstName} ${inventors.secondName}`
+);
+console.table(fullNames);
+
+// Array.sort
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+
+console.table(ordered);
